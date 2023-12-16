90a5429ef8f1044ccb201d67dde2b6000aebacbf
MrElle <cassaralavoro@gmail.com>
2023-12-16 23:53:27 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hello</title>
+</head>
+<body>
+    <!-- Dichiarazione di una variabile php -->
+    <?php $name = 'Ciao mondo PHP' ?>
+
+<!--     Messaggio PHP normale
+    <h1><?php echo "Ciao mondo PHP" ?></h1> 
+-->
+    
+    <!-- Messaggio PHP riprendendo la variabile dichiarata -->
+    <h1><? echo $name ?></h1>
+</body>
+</html>
