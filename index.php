42d2dd8331eeb777d8264b82d30e6384a6f91dff
MrElle <cassaralavoro@gmail.com>
2023-12-18 11:05:04 +0100

    change structure

--- index.php
+++ index.php
@@ -4,16 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hello</title>
+
+    <!-- php section -->
+
+    <?php
+
+    //dichiarazione variabile php
+    $message = 'Ciao mondo PHP';
+    
+    ?>
+
 </head>
 <body>
-    <!-- Dichiarazione di una variabile php -->
-    <?php $name = 'Ciao mondo PHP' ?>
 
 <!--     Messaggio PHP normale
-    <h1><?php echo "Ciao mondo PHP" ?></h1> 
+    <h1><?php echo "Ciao mondo PHP" ?></h1>
 -->
     
     <!-- Messaggio PHP riprendendo la variabile dichiarata -->
-    <h1><? echo $name ?></h1>
+    <h1><?php echo $message; ?></h1>
 </body>
 </html>
